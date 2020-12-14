084b60544503b417923f93b85d1f4e7a5af3922d
RAJESH KUMAR <rajeshkumar@RAJESHs-MacBook-Pro.local>
2020-12-14 23:28:05 +0530

    code format

--- tree.py
+++ tree.py
@@ -16,41 +16,36 @@ class BinaryTree:
         self.nodes = list()
         self.build(data)
 
-    def getNodeFromId(self, node_id):
+    def get_node_from_id(self, node_id):
         for node in self.nodes:
             if node.id == node_id:
                 return node
 
     def build(self, data):
         nodes = data["tree"]["nodes"]
-        self.rootId = data["tree"]["root"]
+        self.root_id = data["tree"]["root"]
         for node in nodes:
             self.nodes.append(Node(**node))
         for node in self.nodes:
-            node.leftNode = self.getNodeFromId(node.left)
-            node.rightNode = self.getNodeFromId(node.right)
-        self.rootNode = self.getNodeFromId(self.rootId)
+            node.leftNode = self.get_node_from_id(node.left)
+            node.rightNode = self.get_node_from_id(node.right)
+        self.rootNode = self.get_node_from_id(self.root_id)
 
 
-
-
-
-def BinaryTreeNodeDepthSum(node, depth=0):
+def binary_tree_node_depth_sum(node, depth=0):
     global Sum
     if not node:
         return 0
     Sum += depth
-    BinaryTreeNodeDepthSum(node.leftNode, depth + 1)
-    BinaryTreeNodeDepthSum(node.rightNode, depth + 1)
+    binary_tree_node_depth_sum(node.leftNode, depth + 1)
+    binary_tree_node_depth_sum(node.rightNode, depth + 1)
     return Sum
 
 
 if __name__ == "__main__":
-    json_string = row = input("Json:")
+    json_string = input("Json:")
     data_dict = json.loads(json_string)
-
     tree = BinaryTree(data_dict)
     Sum = 0
-
-    sum_of_all_node_depths = BinaryTreeNodeDepthSum(tree.rootNode)
+    sum_of_all_node_depths = binary_tree_node_depth_sum(tree.rootNode)
     print("Result - {0}".format(sum_of_all_node_depths))
